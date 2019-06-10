0c8dea1eb91c0c0f4319c829268f92da0e13aef1
dasarsh66 <dasarshmanikonda@gmail.com>
2019-06-10 11:25:32 -0500

    cleaned-up code

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # compareJSON
+npm start to run the project
 A simple comparison of two 2 JSON files ~800 MB
--- compareJSON/compareJSON.js
+++ compareJSON/compareJSON.js
@@ -1,50 +1,48 @@
-/*
-Compare two JSON
-Load key, value pairs,
-*/
-var jsonDiff = require('json-diff')
-
-var readline = require('readline');
-
 var stream = require('stream');
-
-const AWS = require('aws-sdk');
-
-
-const Parser = require('stream-json/Parser');
-const parser = new Parser();
 var fs = require('fs')
-
-
-var diff = require('deep-diff')
-
-
 var observableDiff = require('deep-diff').observableDiff;
-var applyChange = require('deep-diff').applyChange;
+
+var paths = [
+ "legacy_product_id",
+ "master,id",
+ "vendor_name",
+ "vendor_part_number",
+ "allspark_id",
+ "developer_name",
+ "publisher",
+ "category_code",
+ "family_name",
+ "franchise_name",
+ "is_drop_ship_only",
+ "is_drop_ship",
+ "is_embargoed",
+ "street_date",
+ "color",
+ "color_tech",
+ "platform_name",
+ "edition",
+ "carriers",
+ "memory_size",
+ "base_product_sku_upc", // it is in barcodes
+ "tradein_value",
+ "is_tradein",
+ "end_of_life_date",
+ "first_received_date",
+ "eta_date",
+ "preload_date",
+ "web_publisher_name",
+ "brand_name",
+   "size",
+  "display_title",
+  "esrb_rating",
+  "manufacturer_name",
+  "barcodes,upc",
+  "barcodes,base_product_upc"
+]
+  
+var stream = fs.createWriteStream("append.txt", {flags:'a'});
 
 
- items = {};
-// C:/Users/v_dmanikon/Documents/json_Tests/copy_Uncompressed // C:/Users/v_dmanikon/Documents/json_Tests/un_compressed
- 
-
- items[0] = lhs;
- items[1] = rhs;
-
-
-// Before pushing into it
-observableDiff(lhs , rhs , function (d) {
-  // Apply all changes except to the name property...
- // console.log(d.path);
-  //  console.log(d.path[0]);
-  if(paths.includes(d.path[0])){
- // console.log(d);
- // console.log('---------------');
-}
-  else{
-   //  console.log(d.path.toString()); 
-    } 
-
-});
 
 function HashTable(obj)
 {
@@ -129,107 +127,69 @@ function HashTable(obj)
     }
 }
 
-
 var h = new HashTable();
 count = 0;
 var JSONStream = require('JSONStream');
 var es = require('event-stream');
 var bool = false;
 
-
-
-// chrismo 
-var stream1 = fs.createReadStream('C:/Users/v_dmanikon/Documents/Product_chrismo/4.1.0/Product_20190528_144553856.Json', {encoding: 'utf8'}),
+var stream1 = fs.createReadStream('C:/Users/v_dmanikon/Documents/Product_chrismo/latest/Product_20190601_102312405/Product_20190601_102312405.Json', {encoding: 'utf8'}),
     parser1 = JSONStream.parse('skus.*');
 
    stream1.pipe(parser1);
 
 parser1.on('data', function (data) {
-  //  console.log('Printing first doc');
-  
-  if(data.sku=='136108'){
-   console.log('Sku 136108 found  ');
-   // console.log(data);
-   lhs=data;
-}   
-
-  h.setItem(data.sku,data); // whatever you will do with each JSON object
+ 
+  h.setItem(data.sku,data);
   count++;
 });
 parser1.on('end', function (data) {
 
- ///console.log('Memory used in the beggining  ' +process.memoryUsage().rss);
- //console.log('Total Objects '+count);
-  check();
+ check();
 });
 parser1.on('error', function (data) {
 console.log('Error while parsing the chrismo '+data.sku);
 });
 
-var count2=0;
 target =0 ;
 var check =  function () {
-// gismo
-  var stream1 = fs.createReadStream('C:/Users/v_dmanikon/Documents/Product_gismo/product-full_20190524120444040.json', {encoding: 'utf8'}),
+  //              C:/Users/v_dmanikon/Documents/og_product_gismo.json
+// gismo          /C:/Users/v_dmanikon/Documents/Product_gismo/product-full_20190602100006875/product-full_20190602100006875.json
+  var stream1 = fs.createReadStream('C:/Users/v_dmanikon/Documents/Product_gismo/product-full_20190602100006875/product-full_20190602100006875.json', {encoding: 'utf8'}),
       parser1 = JSONStream.parse('skus.*');
-  
+    start = false;
     stream1.pipe(parser1);
   
   parser1.on('data', function (data) {
-      
-    if(target<2){
-        //console.log('Sku 971091');
-      //  console.log(data);
-    target++;
-    }
+        start = false;
     
-
-
-
     if( h.hasItem((data.sku)) == false){
-    // console.log('The following item is not available in GISMO '+ data.sku);
+     console.log('The following item is not available in Chrismo '+ data.sku);
+      stream.write("The following sku is not available in Chrismo "+data.sku+"\n");
      //console.log('Memory usage in the  second '+process.memoryUsage().rss);
     }
-      
-     
-    else if(jsonDiff.diff(data, h.getItem(data.sku))!=undefined){
-        //    console.log('They dont match '+data.sku);
-     // console.log(' Sku number  ' +data.sku+'  changes at   '+'   '+jsonDiff.diffString(data, h.getItem(data.sku)));
-    //  console.log('-----------------');
-    //  console.log('Memory usage in the  second '+process.memoryUsage().rss);
-
-    observableDiff(lhs , rhs , function (d) {
-      // Apply all changes except to the name property...
-     // console.log(d.path);
-      //  console.log(d.path[0]);
-      if(paths.includes(d.path[0])){
-      console.log(d);
-     // console.log('---------------');
-    }
       else{
-       //  console.log(d.path.toString()); 
-        } 
-    
-    });
-
-
-
-    count2++;
-    }
-    else if(jsonDiff.diff(data, h.getItem(data.sku))==undefined){
-          //  console.log('They  match     '+data.sku);
-    }
-    else{
-     //   console.log('in Else '+data.sku);
-    } 
-  });
+        observableDiff(h.getItem(data.sku) , data , function (d) {
+        
+          if(paths.includes(d.path.toString())&&(!d.lhs==""&&d.rhs==null)){
+              if(start==false){
+            stream.write("<------     Errors for sku     "+data.sku+"     ------>"+"\n");
+                      start = true;
+                      }  
+           stream.write(d.path+"-|"+"\n"+"Chrismo -> "+d.lhs+"\n"+"Gismo -> "+d.rhs+"\n");
+        }
+        });
+        
+      }
+  }); 
+  
   parser1.on('error', function (data) {
-  //  console.log('Error while parsing the gismo '+data);
+   console.log('Error while parsing the gismo '+data);
      
     });
     parser1.on('end', function (data) {
-
+      stream.end();
  
-     //   console.log('Memory used in the beggining  ' +process.memoryUsage().rss+'  Objects diff is '+count2);
+      console.log('Memory used in the beggining  ' +process.memoryUsage().rss+'  Objects diff is '+count2);
       });
   };
